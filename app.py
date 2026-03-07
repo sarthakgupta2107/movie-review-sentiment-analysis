c3c2a1ece5acf977bf86e6e4b5d145ae0ba80648
Sarthak Gupta <sarthakgupta2107@gmail.com>
2026-03-07 23:03:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-
 from flask import Flask, render_template, request
 import pickle
+import os
 
 app = Flask(__name__)
 
+# Load model
 model = pickle.load(open("model.pkl", "rb"))
 vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
 
@@ -16,9 +17,10 @@ def predict():
     review = request.form["review"]
     data = vectorizer.transform([review])
     prediction = model.predict(data)[0]
+
     return render_template("index.html", result=prediction)
 
-
+# Render deployment
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
