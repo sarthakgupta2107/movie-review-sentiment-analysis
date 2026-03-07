59dd8ef6c2c255283c5d35e53e41cb1724a74ebf
Sarthak Gupta <sarthakgupta2107@gmail.com>
2026-03-07 22:53:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,5 +18,8 @@ def predict():
     prediction = model.predict(data)[0]
     return render_template("index.html", result=prediction)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
